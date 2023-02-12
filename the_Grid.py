217459083e0241703ab4c3ba1e15b210db573a2b
Hauke <h.uhde@tu-braunschweig.de>
2023-02-12 10:42:17 +0100

    Anfang aber hier ist gerade Kriese :(

--- the_Grid.py
+++ the_Grid.py
@@ -2,3 +2,12 @@
 import numba as nb
 from numba.experimental import jitclass
 from numba.typed import List
+
+@jitclass
+class Grid:
+    xborder : float
+    yborder : float
+    number_x : int
+    number_y : int
+    
+
